710238b886e5b5c1e07958cfa9574f38091eaca7
Neill Jamieson <neill.jamieson@gmail.com>
2024-04-10 08:43:33 +0200

    Add estate shell with init and manifest.

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'Estate tutorial',
+    'description': "Odoo 17 Tutorial - Estate",
+    'depends': [
+        'base'
+    ],
+    'application': True,
+}
